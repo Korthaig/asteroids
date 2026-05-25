e0be64043e1c456b1619d52f30fc4d90be31a8b6
Korthaig <jaime.robb1980@gmail.com>
2026-05-25 08:42:50 -0500

    added collisions

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pygame
+import sys
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from logger import log_event
+
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -28,6 +31,11 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print(f"Game over!")
+                sys.exit()
         for things in drawable:
             things.draw(screen)
         pygame.display.flip()
